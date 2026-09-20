fb94a1852c7bc83c6b4f46d41d859b3b8ee395fc
nazar2012 <133564811+nazar2012@users.noreply.github.com>
2026-09-20 11:28:14 +0300

    movie

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,12 +1,13 @@
-import { Link } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
 
 function MovieList({ movies }) {
+    const location = useLocation()
     return (
         <ul>
             {movies.map((movie) => {
                 return (
                     <li key={movie.id}>
-                        <Link to={`/movies/${movie.id}`}>{movie.title}</Link>
+                        <Link state={location} to={`/movies/${movie.id}`}>{movie.title}</Link>
                     </li>
                 )
             })}
--- src/pages/MovieDetails/MovieDetails.jsx
+++ src/pages/MovieDetails/MovieDetails.jsx
@@ -1,10 +1,45 @@
-import { useParams } from "react-router-dom"
+import { useParams, useLocation, Link } from "react-router-dom"
+import { useEffect, useState } from "react";
+import { getMovieDetails } from "../../api";
 
 function MovieDetails() {
     const { movieId } = useParams()
-    console.log(movieId);
+    const location = useLocation()
+    const [movie, setMovie] = useState({})
+
+    useEffect(() => {
+        async function fetchMovie() {
+            const data = await getMovieDetails(movieId)
+            setMovie(data)
+        }
+        fetchMovie()
+    }, [movieId])
+
+    console.log(location);
+
+    const imageUrl = `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
+    const deafultImg = "https://static.vecteezy.com/system/resources/thumbnails/022/059/000/small_2x/no-image-available-icon-vector.jpg"
+
     return (
-        <h1>MovieDetails page</h1>
+        <div>
+            <Link to={location.state.pathname || "/"}>Go back</Link>
+            {/* <img src={imageUrl} alt={movie.title} /> */}
+            {movie.poster_path ? (
+                <img src={imageUrl} alt={movie.title} />
+            ) : <img src={deafultImg} alt={movie.title} />}
+            <div className="conteiner">
+                <h1>{movie.title}</h1>
+                <p></p>
+                <h2>Overview</h2>
+                <p>{movie.overview}</p>
+                <h3>Genres</h3>
+                <ul>{movie.genres?.map((item) => {
+                    return (
+                        <li key={item.id}>{item.name}</li>
+                    )
+                })}</ul>
+            </div>
+        </div>
     )
 }
 
